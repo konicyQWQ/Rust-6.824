632234261232aa1d8d0cae41666c507d0b71a3d5
konicyqwq <konicy@outlook.com>
2024-12-19 15:05:58 +0800

    save lab1 code

--- src/mr/coordinator.rs
+++ src/mr/coordinator.rs
@@ -1,6 +1,7 @@
 use futures::{future, prelude::*};
 use std::net::{IpAddr, Ipv4Addr};
-use std::sync::Arc;
+use std::sync::{atomic, Arc, Mutex};
+use std::time::{Duration, Instant};
 use tarpc::context::Context;
 use tarpc::server::{self, incoming::Incoming, Channel};
 use tarpc::tokio_serde::formats::Json;
@@ -46,6 +47,7 @@ pub trait CoordinatorRPCService {
     async fn call_example(args: String) -> String;
 
     // add your rpc functions here
+    async fn get_task(id: u32, status: WorkerStatus) -> Task;
 }
 
 impl CoordinatorRPCService for CoordinatorServer {
@@ -54,6 +56,18 @@ impl CoordinatorRPCService for CoordinatorServer {
     }
 
     // implement your rpc functions here
+    async fn get_task(self, _: Context, id: u32, status: WorkerStatus) -> Task {
+        match status {
+            WorkerStatus::Finish => self.coordinator.finish_task(id),
+            _ => {}
+        }
+
+        if self.coordinator.done() {
+            Task::Finish
+        } else {
+            self.coordinator.allocate_task(id)
+        }
+    }
 }
 
 pub struct Coordinator {
@@ -62,18 +76,150 @@ pub struct Coordinator {
     // !!!NOTICE!!!
     // the CoordinatorServer contains Arc<Coordinator> which means the Coordinator is immutable.
     // you can use Mutex<...> or atomic::... to ensure the Coordinator is thread-safe and interior mutable.
+    n_reduce: u32,
+    done: atomic::AtomicBool,
+    tasks: Mutex<Vec<WaitTaskWithWorker>>,
 }
 
 impl Coordinator {
     pub fn new(files: &[String], n_reduce: u32) -> Self {
         // your coordinator implementation here
-        unimplemented!()
+        // unimplemented!()
+
+        Coordinator {
+            n_reduce,
+            done: atomic::AtomicBool::new(false),
+            tasks: Mutex::new(
+                files
+                    .iter()
+                    .map(|file| WaitTaskWithWorker {
+                        task: WaitTask::Map(file.to_string()),
+                        worker_id: None,
+                        start_time: None,
+                    })
+                    .collect(),
+            ),
+        }
     }
 
     pub fn done(&self) -> bool {
         // your coordinator implementation here
-        unimplemented!()
+        // unimplemented!()
+
+        self.done.load(atomic::Ordering::SeqCst)
     }
 
     // add other functions, you can only use immutable self
+
+    pub fn set_done(&self) {
+        self.done.store(true, atomic::Ordering::SeqCst);
+    }
+
+    pub fn finish_task(&self, worker_id: u32) {
+        let mut tasks = self.tasks.lock().unwrap();
+
+        // find this task
+        if let Some(pos) = tasks
+            .iter()
+            .position(|task| task.worker_id == Some(worker_id))
+        {
+            // remove it
+            let task = tasks.swap_remove(pos);
+            let task_name = match &task.task {
+                WaitTask::Map(_) => "map",
+                WaitTask::Reduce(_) => "reduce",
+            };
+
+            // rename tmp_file to file
+            for i in 0..self.n_reduce {
+                match &task.task {
+                    WaitTask::Map(filename) => {
+                        let tmp_file =
+                            format!("{}-{}-{}-{}-tmp", i, filename, worker_id, task_name);
+                        let file = format!("{}-{}-{}-{}", i, filename, worker_id, task_name);
+                        std::fs::rename(&tmp_file, &file).unwrap_or_default();
+                    }
+                    WaitTask::Reduce(_) => {
+                        let tmp_file = format!("{}-{}-{}-tmp", i, worker_id, task_name);
+                        let file = format!("mr-out-{}-{}-{}", i, worker_id, task_name);
+                        std::fs::rename(&tmp_file, &file).unwrap_or_default();
+                    }
+                }
+            }
+
+            // if map task all done, add reduce task
+            if task_name == "map" && tasks.is_empty() {
+                (0..self.n_reduce)
+                    .map(|i| WaitTaskWithWorker {
+                        task: WaitTask::Reduce(i),
+                        worker_id: None,
+                        start_time: None,
+                    })
+                    .for_each(|task| tasks.push(task));
+            }
+
+            // if reduce task all done, whole task done
+            if task_name == "reduce" && tasks.is_empty() {
+                self.set_done();
+            }
+        }
+    }
+
+    pub fn allocate_task(&self, worker_id: u32) -> Task {
+        let mut tasks = self.tasks.lock().unwrap();
+
+        tasks
+            .iter_mut()
+            .find(|task| {
+                task.worker_id == None
+                    || task.start_time.unwrap().elapsed() > Duration::from_secs(10)
+            })
+            .map_or(Task::Wait, |task| {
+                task.worker_id = Some(worker_id);
+                task.start_time = Some(Instant::now());
+                match &task.task {
+                    WaitTask::Map(file) => Task::Map(MapTask {
+                        input_file: file.to_string(),
+                        reduce_number: self.n_reduce,
+                    }),
+                    WaitTask::Reduce(idx) => Task::Reduce(ReduceTask { reduce_idx: *idx }),
+                }
+            })
+    }
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub enum WorkerStatus {
+    Empty,
+    Finish,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct MapTask {
+    pub input_file: String,
+    pub reduce_number: u32,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct ReduceTask {
+    pub reduce_idx: u32,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub enum Task {
+    Map(MapTask),
+    Reduce(ReduceTask),
+    Wait,
+    Finish,
+}
+
+enum WaitTask {
+    Map(String),
+    Reduce(u32),
+}
+
+struct WaitTaskWithWorker {
+    task: WaitTask,
+    worker_id: Option<u32>,
+    start_time: Option<Instant>,
 }
--- src/mr/worker.rs
+++ src/mr/worker.rs
@@ -1,5 +1,6 @@
-use crate::mr::coordinator::CoordinatorRPCServiceClient;
-use crate::mrapps::wc::MapReduce;
+use crate::mr::coordinator::{CoordinatorRPCServiceClient, Task, WorkerStatus};
+use crate::mrapps::wc::{KeyValue, MapReduce};
+use itertools::Itertools;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::Hasher;
 use std::net::{IpAddr, Ipv4Addr};
@@ -41,7 +42,7 @@ impl Worker {
 
     pub async fn start(&self) -> anyhow::Result<()> {
         // your worker implementation here
-        unimplemented!()
+        // unimplemented!()
 
         // self.map_reduce contains the map function and the reduce function.
         // you should use them to implement worker process.
@@ -51,5 +52,95 @@ impl Worker {
         // rpc call example
         //
         // self.rpc_client.call_example(Context::current(), args).await?;
+
+        let worker_id = std::process::id();
+        let mut status = WorkerStatus::Empty;
+        loop {
+            let task = self
+                .rpc_client
+                .get_task(Context::current(), worker_id, status.clone())
+                .await?;
+
+            match task {
+                Task::Map(task) => {
+                    let content = tokio::fs::read_to_string(&task.input_file).await?;
+                    let kvs = self.map_reduce.map(&task.input_file, &content);
+                    let mut buckets: Vec<Vec<KeyValue>> =
+                        vec![Vec::new(); task.reduce_number as usize];
+                    for kv in kvs {
+                        buckets[(i_hash(kv.key.as_str()) % task.reduce_number as u64) as usize]
+                            .push(kv);
+                    }
+                    for i in 0..task.reduce_number {
+                        let file_name = format!("{}-{}-{}-map-tmp", i, task.input_file, worker_id);
+                        let content = buckets[i as usize]
+                            .iter()
+                            .map(|kv| format!("{} {}", kv.key, kv.value))
+                            .join("\n");
+                        tokio::fs::write(file_name, content.as_bytes()).await?;
+                    }
+
+                    status = WorkerStatus::Finish
+                }
+                Task::Reduce(task) => {
+                    let dir_entries = tokio::fs::read_dir(".").await?;
+                    let mut files = Vec::new();
+
+                    tokio::pin!(dir_entries);
+                    while let Some(entry) = dir_entries.next_entry().await? {
+                        let file_name = entry.file_name();
+                        if let Some(name) = file_name.to_str() {
+                            if name.starts_with(&format!("{}-", task.reduce_idx))
+                                && name.ends_with("-map")
+                            {
+                                files.push(entry.path());
+                            }
+                        }
+                    }
+
+                    let mut kvs: Vec<KeyValue> = Vec::new();
+                    for path in files {
+                        let content = tokio::fs::read_to_string(&path).await?;
+                        content.lines().for_each(|line| {
+                            let kv: Vec<&str> = line.split_whitespace().collect();
+                            assert_eq!(kv.len(), 2, "{}, {:?}", &line, &path);
+                            kvs.push(KeyValue {
+                                key: kv[0].to_string(),
+                                value: kv[1].to_string(),
+                            })
+                        })
+                    }
+
+                    let mut out_kvs: Vec<KeyValue> = Vec::new();
+
+                    kvs.sort();
+                    for (key, chunk) in &kvs.iter().chunk_by(|kv| &kv.key) {
+                        let values: Vec<String> = chunk.map(|kv| kv.value.clone()).collect();
+                        let reduce_output = self.map_reduce.reduce(key, &values);
+
+                        out_kvs.push(KeyValue {
+                            key: key.to_string(),
+                            value: reduce_output,
+                        })
+                    }
+
+                    let file_name = format!("{}-{}-reduce-tmp", task.reduce_idx, worker_id);
+                    let content = out_kvs
+                        .iter()
+                        .map(|kv| format!("{} {}", kv.key, kv.value))
+                        .join("\n");
+                    tokio::fs::write(file_name, content.as_bytes()).await?;
+
+                    status = WorkerStatus::Finish
+                }
+                Task::Wait => {
+                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                    status = WorkerStatus::Empty
+                }
+                Task::Finish => break,
+            }
+        }
+
+        Ok(())
     }
 }
